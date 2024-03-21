5297b71bd661d3864010f658081aac589d2eb474
tolarmi <142987584+tolarmi@users.noreply.github.com>
2024-03-21 15:05:25 -0600

    Update homework 3-1.py

--- homework 3-1.py
+++ homework 3-1.py
@@ -1,11 +1,16 @@
 from datetime import datetime
 
 def get_days_from_today(date):
+    try:
+        
+        input_date = datetime.strptime(date, '%Y-%m-%d')
+      
+        current_date = datetime.today()
+        
+        difference = input_date - current_date
+        
+        return difference.days
     
-    input_date = datetime.strptime(date, '%Y-%m-%d')
-    
-    current_date = datetime.today()
-    
-    difference = current_date - input_date
-    
-    return difference.days
+    except ValueError:
+        print("Неправильний формат вхідних даних. Потрібно використовувати формат 'РРРР-ММ-ДД'.")
+        return None
